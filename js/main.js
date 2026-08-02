d7f0a659b65358119c9dde9a6bf45fb74cee9902
Ani Mazzucco <ciro040516@gmail.com>
2026-08-02 20:25:16 -0300

    cambios

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
-
-const nombre = prompt('Ingrese su nombre');
-const apellido = prompt('Ingrese su apellido');
-let localidad = prompt('Ingrese su localidad');
-let nacimiento = parseInt (prompt('Ingrese su año de nacimiento'));
-let edadactual = parseInt (prompt('Ingrese el año actual para calcular su edad'));
+const nombre = prompt('Bienvenido a la página de Proyectos Di&Bere. Para continuar con la carga de datos puede dar click en la opción ACEPTAR o CANCELAR en caso de no seguir con el procedimiento. Ingrese su nombre:');
+const apellido = prompt('Ingrese su apellido:');
+let localidad = prompt('Ingrese su localidad:');
+let celular = parseInt (prompt('Ingrese su número de celular (sin puntos):'));
+let nacimiento = parseInt (prompt('Ingrese su año de nacimiento (sin puntos):'));
+let edadactual = parseInt (prompt('Ingrese el año actual para calcular su edad (sin puntos):'));
 const nombrecompleto = nombre + ' ' + apellido;
 edadactual= 2026 - nacimiento;
 console.log(nombrecompleto);
 console.log(localidad);
+console.log(celular);
 console.log(nacimiento);
 console.log(edadactual);
-alert(`Bienvenido a la base de datos de Proyectos Di&Bere. Los datos ingresados son los siguientes: 
+alert(`Bienvenido a la base de datos de Proyectos Di&Bere. La información ingresada es la siguiente: 
     Nombre Completo: ${nombrecompleto} 
     Localidad: ${localidad} 
+    Celular: ${celular}
     Nacimiento: ${nacimiento} 
     Edad Actual: ${edadactual} 
     Para finalizar haga click en el botón de Aceptar`);
